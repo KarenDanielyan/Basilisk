4c7fd5ab03e0408284cf7436330e9b90e29aa400
KarenDanielyan <karendanielyan66@gmail.com>
2022-05-03 15:23:56 +0400

    Win32 platform layer

--- testbed/src/main.c
+++ testbed/src/main.c
@@ -1,6 +1,9 @@
 #include <core/logger.h>
 #include <core/asserts.h>
 
+// TODO: Test
+#include <platform/platform.h>
+
 int main(void) 
 {
     BFATAL("This is a fatal error: %f", 3.14f);
@@ -10,7 +13,12 @@ int main(void)
     BDEBUG("This is a debug: %f", 3.14f);
     BTRACE("This is a trace: %f", 3.14f);
 
-    BASSERT(1 == 0);
-
+    platform_state state;
+    if(platform_startup(&state, "Basilisk Engine Testbed", 100, 100, 1280, 720)){
+        while(TRUE){
+            platform_pump_messages(&state);
+        }
+    }
+    platform_shutdown(&state);
     return 0;
 }
